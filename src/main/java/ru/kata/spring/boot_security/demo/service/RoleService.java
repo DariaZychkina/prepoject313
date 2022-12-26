e307fa14d6bbf26d58140acbea4cbbf5c3132aa3
DariaZychkina <99396496+DariaZychkina@users.noreply.github.com>
2022-12-26 11:31:47 +0300

    Update RoleService.java

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -5,6 +5,6 @@ import ru.kata.spring.boot_security.demo.model.Role;
 import java.util.Collection;
 
 public interface RoleService {
-    Collection<Role> getAllRoles();
+    Set<Role> getAllRoles();
 
 }
